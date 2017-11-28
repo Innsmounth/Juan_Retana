a2e9b0596d4e44218d48e92692a2fef4c288b74f
T-102 <T-102@PC242>
2017-11-27 19:39:25 -0600

    Conexion Completa

--- lenguajes/src/conexion/ProbarConexion.java
+++ lenguajes/src/conexion/ProbarConexion.java
@@ -5,22 +5,37 @@ public class ProbarConexion
 {
     public static void main(String[] args)
     {
+        Connection con = null;
         try
         {
-        Conexion.conectarse("root", "");
+            Conexion.conectarse("roo", "");
+            System.out.println("Te conectaste");
         }
         catch (ClassNotFoundException e)
         {
-        
+            System.out.println("No se cargo ben el driver");
         }
         catch(SQLException e)
         {
-        
+            System.out.println("Error en SQL " + e.getMessage());
         }
         finally
         {
-            
+            try
+            {
+                if (con != null) con.close();
+                System.out.println("Ya se cerro la conexion");
+                
+            }
+                catch (SQLException ex)
+            {
+                
+                
+            }
         }
-    
+        
     }
-}
+    
+ }
+
+    
